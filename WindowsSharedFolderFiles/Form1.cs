4b336de25c330cea2bf35b2b98cc068547c56036
boroogrammer <boroo_c@yahoo.com>
2015-02-02 09:57:33 +0800

    first commit

---
+++ WindowsSharedFolderFiles/Form1.cs
@@ -0,0 +1,34 @@
+﻿using ExtremeMirror;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsSharedFolderFiles
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo dir = new DirectoryInfo(textBox1.Text);
+
+            // ene class ni windows shared folder handalt hiih erh uusgej ogdog
+            PinvokeWindowsNetworking.connectToRemote(textBox1.Text, "username", "password");
+
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                listBox1.Items.Add(file.Name);
+            }
+        }
+    }
+}
